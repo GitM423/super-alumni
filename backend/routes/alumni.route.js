c9070de7224480e857c1374a9f1fdb6b6a5a29d6
GitM423 <matthias.p.hoeh@gmail.com>
2020-10-19 11:09:32 +0200

    test

--- backend/routes/alumni.route.js
+++ backend/routes/alumni.route.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-let Alumni = require("../models/user.model");
+let Alumni = require("../models/alumni.model");
 
 router.route("/").get((req, res) => {
   Alumni.find()
