ef23513e4f101257583143a984b8ac3cbe18f001
emabytes <emadavor22@gmail.com>
2020-10-21 13:48:27 +0200

    test

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a4811ceecdf39a4480af";
+/******/ 	var hotCurrentHash = "fcc1b9e6e68662d2a3de";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
